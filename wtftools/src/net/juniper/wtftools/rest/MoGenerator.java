bb199b85c8b7d0484528bbcf3060f83a78b29d9c
juntaod <juntao.d@outlook.com>
2013-12-11 17:10:02 -0800

    k

--- wtftools/src/net/juniper/wtftools/rest/MoGenerator.java
+++ wtftools/src/net/juniper/wtftools/rest/MoGenerator.java
@@ -2,9 +2,14 @@ package net.juniper.wtftools.rest;
 
 import java.lang.reflect.Method;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.Iterator;
 import java.util.List;
 
+import net.juniper.wtftools.WtfToolsActivator;
 import net.juniper.wtftools.core.WtfProjectCommonTools;
+
+import org.apache.commons.lang.StringUtils;
 
 public class MoGenerator extends AbstractServiceGenerator{
 
@@ -27,26 +32,110 @@ public class MoGenerator extends AbstractServiceGenerator{
 
 	@Override
 	protected String replaceFile(String template) {
-		getMethodDesc();
+		List<MethodDesc> methodDesc = getMethodDesc();
+		String propStr = getPropsStr(methodDesc);
+		String fieldStr = getFieldStr(methodDesc);
+		String methodStr = getMethodStr(methodDesc);
 		template = template.replace("#MO_PACKAGE_NAME#", getPackageName().replaceAll("/", "."));
 		template = template.replace("#SERVICE_NAME#", getSimpleName().toLowerCase());
 		return template;
 	}
 
-	private void getMethodDesc() {
+	private String getFieldStr(List<MethodDesc> methodDesc) {
+		StringBuffer buf = new StringBuffer();
+		Iterator<MethodDesc> it = methodDesc.iterator();
+		while(it.hasNext()){
+			MethodDesc method = it.next();
+			buf.append("\tprivate ");
+			buf.append(getTypeString(method.fullType)).append(" ");
+			buf.append(method.name);
+			if(it.hasNext())
+				buf.append(",");
+		}
+		return buf.toString();
+	}
+
+	private String getTypeString(String fullType){
+		if(fullType.equals(String.class.getName()))
+			return "String";
+		else if(fullType.equals(Integer.class.getName()))
+			return "Integer";
+		return "String";
+	}
+	
+	private String getMethodStr(List<MethodDesc> methodDesc) {
+		StringBuffer buf = new StringBuffer();
+		Iterator<MethodDesc> it = methodDesc.iterator();
+		while(it.hasNext()){
+			MethodDesc method = it.next();
+			buf.append("\n\tpublic void set").append(StringUtils.capitalize(method.name)).append("(");
+			buf.append(getTypeString(method.fullType)).append(" ");
+			buf.append(method.name);
+			buf.append("){\n");
+			buf.append("\t\t").append("return ");
+			buf.append(method.name).append(";\n");
+			buf.append("\t}\n");
+			
+			buf.append("\n\tpublic ");
+			
+			//void get").append(StringUtils.capitalize(method.name)).append("(");
+			if(method.fullType.equals(String.class.getName()))
+				buf.append("String ");
+			else if(method.fullType.equals(Integer.class.getName()))
+				buf.append("Integer ");
+			buf.append(method.name);
+			buf.append("){\n");
+			buf.append("\t\t").append("return ");
+			buf.append(method.name).append(";\n");
+			buf.append("\t}\n");
+			
+			if(it.hasNext())
+				buf.append(",");
+		}
+		return buf.toString();
+	}
+
+	private String getPropsStr(List<MethodDesc> methodDesc) {
+		StringBuffer buf = new StringBuffer();
+		Iterator<MethodDesc> it = methodDesc.iterator();
+		while(it.hasNext()){
+			MethodDesc method = it.next();
+			buf.append(method.name);
+			if(it.hasNext())
+				buf.append(",");
+		}
+		return buf.toString();
+	}
+
+	private List<MethodDesc> getMethodDesc() {
 		try {
 			List<MethodDesc> descList = new ArrayList<MethodDesc>();
 			Class c = Class.forName(entityPath, true, WtfProjectCommonTools.getCurrentProjectClassLoader());
 			Method[] mds = c.getDeclaredMethods();
 			for(Method m : mds){
-				String name = m.getName();
-				if(name.startsWith("get") && m.getParameterTypes().length == 0){
+				String methodName = m.getName();
+				if(methodName.startsWith("get") && m.getParameterTypes().length == 0){
 					MethodDesc desc = new MethodDesc();
+					String name = methodName.substring("get".length()).toLowerCase();
+					desc.name = name;
+					Class<?> rtC = m.getReturnType();
+					if(rtC.equals(String.class)){
+						desc.type = "String";
+					}
+					else if(rtC.equals(Integer.class)){
+						desc.type = "Integer";
+					}
+					else if(rtC.equals(Date.class)){
+						desc.type = "Date";
+					}
+					descList.add(desc);
 				}
 			}
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			return descList;
+		} 
+		catch (Exception e) {
+			WtfToolsActivator.getDefault().logError(e);
+			return null;
 		}
 	}
 
